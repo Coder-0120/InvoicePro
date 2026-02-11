59d3ea57fdc634f901e195237169a5e64fafdade
Anshul Verma <av4862187@gmail.com>
2026-02-11 07:15:23 +0530

    Use env variable for backend URL

--- server/index.js
+++ server/index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(
   cors({
     origin: [
-    //   "http://localhost:3000",
+      "http://localhost:3000",
       "https://invoice-pro-sepia.vercel.app"
     ],
     credentials: true
